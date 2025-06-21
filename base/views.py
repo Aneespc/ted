8e2a32e047e5524c8533676647a6e041222998b6
Aneespc <pcanees26@gmail.com>
2025-06-21 23:55:04 +0530

    tedx

---
+++ base/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Room
+
+# Create your views here.
+
+# rooms=[
+#     {'id':1,'name':"python"},
+#     {'id':2,'name':"design"},
+#     {'id':3,'name':"developing"},
+# ]
+
+def home(request):
+    rooms=Room.objects.all()
+    context={"rooma":rooms}
+    return render(request, 'base/home.html',context)
+
+def room(request, pk):
+    room=Room.objects.get(id=pk)
+    # for i in rooms:
+    #     if i['id']==int(pk):
+    #         room=i
+    context={"room":room}
+    return render(request, 'base/room.html',context)
---
+++ members/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+from . import views
+
+
+urlpatterns = [
+    path('login_user',views.login_user,name='login')
+
+]
